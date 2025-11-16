4b26c85e51cf70600bb27ca9047ab6faca88d99a
sri <sriramrmk@gmail.com>
2025-11-16 14:42:14 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.21.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
+
+
+resource "aws_vpc" "terraform" {
+  cidr_block       = "10.10.0.0/16"
+  
+  tags = {
+    Name = "practice_tf"
+  }
+}
