fdeb6faf5c9319186b54b48fd094a8f6475da7d4
srikvp <93551165+srikvp@users.noreply.github.com>
2025-11-17 14:21:52 -0500

    implicit dependency

--- AWS/main.tf
+++ AWS/main.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.21.0"
     }
   }
@@ -15,8 +15,8 @@ provider "aws" {
 
 
 resource "aws_vpc" "terraform" {
-  cidr_block       = "10.10.0.0/16"
-  
+  cidr_block = "10.10.0.0/16"
+
   tags = {
     Name = "practice_tf"
   }
--- Azure/main.tf
+++ Azure/main.tf
@@ -2,7 +2,7 @@
 provider "azurerm" {
   # Configuration options
   features {
-    
+
   }
 }
 
@@ -12,10 +12,10 @@ resource "azurerm_resource_group" "fromtf" {
 }
 
 resource "azurerm_storage_account" "store" {
-  name = "fromtfstorage"
-  resource_group_name = azurerm_resource_group.fromtf.name
-  location = azurerm_resource_group.fromtf.location
-  account_tier = "Standard"
+  name                     = "fromtfstorage"
+  resource_group_name      = azurerm_resource_group.fromtf.name
+  location                 = azurerm_resource_group.fromtf.location
+  account_tier             = "Standard"
   account_replication_type = "GRS"
-  depends_on = [ azurerm_resource_group.fromtf ]
+  depends_on               = [azurerm_resource_group.fromtf]
 }
