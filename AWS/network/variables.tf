9b8d00cd962cb6c9940ce4328c757afbd0907037
sri <sriramrmk@gmail.com>
2025-11-17 22:08:50 -0500

    introducing objects

--- AWS/network/variables.tf
+++ AWS/network/variables.tf
@@ -6,94 +6,87 @@ variable "region" {
   default     = "us-east-1" #the default value that will be passed when a value is not passed
 }
 
-variable "vpc_name" {
-  type        = string
-  description = "name of the vpc"
-  default     = "noptf"
-}
-
-variable "vpc_cidr" {
-  type        = string
-  description = "this is the cidr value of the vpc"
-  default     = "10.0.0.0/16"
+variable "vpc_info" {
+  description = "vpc info"
+  type        = object({
+    name = string
+    cidr = string
+  })
+  
+  default = {
+    name = "noptf"
+    cidr = "10.0.0.0/16" 
+  }
 }
 
 # Subnet info
 
-## Subnet names
-variable "app1_subnet_name" {
-  type        = string
-  description = "name of app1 subnet"
-  default     = "app1"
+## App1 Subnet info
+
+variable "app1_subnet_info" {
+  description = "app1 subnet info"
+  type        = object({
+    name = string
+    cidr = string
+    az = string
+  })
+  
+  default = {
+    name = "app1"
+    cidr = "10.0.1.0/24"
+    az = "us-east-1a"
+  }
 }
 
-variable "app2_subnet_name" {
-  type        = string
-  description = "name of app 2 subnet"
-  default     = "app2"
+
+## App2 subnet info
+
+variable "app2_subnet_info" {
+  description = "app2 subnet info"
+  type        = object({
+    name = string
+    cidr = string
+    az = string
+  })
+  
+  default = {
+    name = "app2"
+    cidr = "10.0.2.0/24"
+    az = "us-east-1b"
+  }
 }
 
-variable "db1_subnet_name" {
-  type        = string
-  description = "name of db1 subnet"
-  default     = "db1"
+## Db1 subnet info
+
+variable "db1_subnet_info" {
+  description = "db1 subnet info"
+  type        = object({
+    name = string
+    cidr = string
+    az = string
+  })
+  
+  default = {
+    name = "db1"
+    cidr = "10.0.11.0/24"
+    az = "us-east-1a"
+  }
 }
 
-variable "db2_subnet_name" {
-  type        = string
-  description = "name of db2 subnet"
-  default     = "db2"
+
+## db2 subnet info
+
+variable "db2_subnet_info" {
+  description = "db2 subnet info"
+  type        = object({
+    name = string
+    cidr = string
+    az = string
+  })
+  
+  default = {
+    name = "db2"
+    cidr = "10.0.12.0/24"
+    az = "us-east-1b"
+  }
 }
-
-## Subnet AZ info
-
-variable "app1_subnet_az" {
-  type        = string
-  description = "az where app1 resides"
-  default     = "us-east-1a"
-}
-
-variable "app2_subnet_az" {
-  type        = string
-  description = "az where app1 resides"
-  default     = "us-east-1b"
-}
-
-variable "db1_subnet_az" {
-  type        = string
-  description = "az where db1 resides"
-  default     = "us-east-1a"
-}
-
-variable "db2_subnet_az" {
-  type        = string
-  description = "az where db2 resides"
-  default     = "us-east-1b"
-}
-
-## Subnet CIDR
-
-variable "app1_cidr" {
-  type        = string
-  description = "app1 subnet"
-  default     = "10.0.1.0/24"
-}
-
-variable "app2_cidr" {
-  type        = string
-  description = "app2 subnet"
-  default     = "10.0.2.0/24"
-}
-
-variable "db1_cidr" {
-  type        = string
-  description = "db1 subnet"
-  default     = "10.0.11.0/24"
-}
-
-variable "db2_cidr" {
-  type        = string
-  description = "db2 subnet"
-  default     = "10.0.12.0/24"
-}
-
