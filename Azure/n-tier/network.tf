dd290ec3367a0d974179789f32e8186f232274bb
sri <sriramrmk@gmail.com>
2025-11-24 11:09:22 -0500

    creating network.tf

---
+++ Azure/n-tier/network.tf
@@ -0,0 +1,24 @@
+# Creating a Virtual network for ntier
+resource "azurerm_virtual_network" "ntier" {
+  name                = var.network_info.name
+  location            = azurerm_resource_group.ntier.location
+  resource_group_name = azurerm_resource_group.ntier.name
+  address_space       = [var.network_info.address_space]
+  tags = {
+    environment = "Dev"
+  }
+  #explicit dependency
+  depends_on = [azurerm_resource_group.ntier]
+}
+
+# Creating subnets using count.
+# Count value is determined by number of subnets defined in network_info variable
+resource "azurerm_subnet" "ntier" {
+  count                = length(var.network_info.subnets)
+  name                 = var.network_info.subnets[count.index].name
+  resource_group_name  = azurerm_resource_group.ntier.name
+  virtual_network_name = azurerm_virtual_network.ntier.name
+  address_prefixes     = [var.network_info.subnets[count.index].address_space]
+  #explicit dependency
+  depends_on = [azurerm_resource_group.ntier, azurerm_virtual_network.ntier]
+}
