65f977128920ef7b124aed05e15a7004623ebe19
sri <sriramrmk@gmail.com>
2025-11-30 14:58:26 -0500

    add web urls as output

--- AWS/n-tier/outputs.tf
+++ AWS/n-tier/outputs.tf
@@ -1,3 +1,18 @@
 
 # web url for web vms
 
+output "web_urls" {
+  value = [
+    for i in range(length(aws_instance.web_instance)) :
+    format("http://%s", aws_instance.web_instance[i].public_ip)
+  ]
+}
+
+# alternate method
+
+# output "web_urls" {
+#   value = [
+#     for inst in aws_instance.web_instance :
+#     format("http://%s", inst.public_ip)
+#   ]
+# }
